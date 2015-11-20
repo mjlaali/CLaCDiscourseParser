f0a9874480e4234a8f78d92802d583e8a72c885b
Majid Laali <mjlaali@gmail.com>
2015-11-20 15:23:04 -0500

    add the DC disambiguation project

---
+++ clac.discourse.parser/parser.dc-disambiguation/src/main/java/org/parser/dc/disambiguation/App.java
@@ -0,0 +1,13 @@
+package org.parser.dc.disambiguation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
