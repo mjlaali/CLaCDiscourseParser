bf73b9f644b644016f46995a522a492261a89771
Majid Laali <mjlaali@gmail.com>
2016-11-11 13:56:48 -0500

    move getEngineDescription to GenericClassifierLabeler

--- discourse.parser.parent/tools/src/main/java/ca/concordia/clac/ml/classifier/GenericClassifierLabeller.java
+++ discourse.parser.parent/tools/src/main/java/ca/concordia/clac/ml/classifier/GenericClassifierLabeller.java
@@ -1,19 +1,28 @@
 package ca.concordia.clac.ml.classifier;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.initializable.InitializableFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.cleartk.ml.CleartkAnnotator;
 import org.cleartk.ml.CleartkProcessingException;
+import org.cleartk.ml.DataWriter;
 import org.cleartk.ml.Feature;
 import org.cleartk.ml.Instance;
+import org.cleartk.ml.jar.DefaultDataWriterFactory;
+import org.cleartk.ml.jar.GenericJarClassifierFactory;
 
 public class GenericClassifierLabeller<CLASSIFIER_OUTPUT, INSTANCE_TYPE>
 		extends CleartkAnnotator<CLASSIFIER_OUTPUT> {
@@ -57,5 +66,38 @@ public class GenericClassifierLabeller<CLASSIFIER_OUTPUT, INSTANCE_TYPE>
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
 		genericClassifier.process(aJCas);
+	}
+	
+	public static <CLASSIFIER_OUTPUT, INSTANCE_TYPE> AnalysisEngineDescription getWriterDescription(
+			Class<? extends ClassifierAlgorithmFactory<CLASSIFIER_OUTPUT, INSTANCE_TYPE>> classifierAlgorithmFactoryCls,
+					@SuppressWarnings("rawtypes") Class<? extends DataWriter> dataWriterCls, File outputDirectory, Object... otherParams) throws ResourceInitializationException{
+		List<Object> params = new ArrayList<>();
+		params.addAll(Arrays.asList(
+				GenericClassifierLabeller.PARAM_LABELER_CLS_NAME, classifierAlgorithmFactoryCls.getName(), 
+				CleartkAnnotator.PARAM_IS_TRAINING, true, 
+				DefaultDataWriterFactory.PARAM_DATA_WRITER_CLASS_NAME, dataWriterCls.getName(),
+				DefaultDataWriterFactory.PARAM_OUTPUT_DIRECTORY, outputDirectory) 
+				);
+		params.addAll(Arrays.asList(otherParams));
+
+		return AnalysisEngineFactory.createEngineDescription(StringClassifierLabeller.class,
+				params.toArray(new Object[params.size()])
+				);
+	}
+
+	public static <CLASSIFIER_OUTPUT, INSTANCE_TYPE> AnalysisEngineDescription getClassifierDescription(
+			Class<? extends ClassifierAlgorithmFactory<CLASSIFIER_OUTPUT, INSTANCE_TYPE>> classifierAlgorithmFactoryCls, URL modelUrl, Object... otherParams) throws ResourceInitializationException{
+		List<Object> params = new ArrayList<>();
+		params.addAll(Arrays.asList(
+				GenericClassifierLabeller.PARAM_LABELER_CLS_NAME, classifierAlgorithmFactoryCls.getName(), 
+				CleartkAnnotator.PARAM_IS_TRAINING, false, 
+				GenericClassifierLabeller.PARAM_PARALLEL_CLASSIFICATION, false,
+				GenericJarClassifierFactory.PARAM_CLASSIFIER_JAR_PATH, modelUrl)
+				);
+		params.addAll(Arrays.asList(otherParams));
+
+		return AnalysisEngineFactory.createEngineDescription(StringClassifierLabeller.class, 
+				params.toArray(new Object[params.size()])
+				);
 	}
 }
--- discourse.parser.parent/tools/src/main/java/ca/concordia/clac/ml/classifier/StringClassifierLabeller.java
+++ discourse.parser.parent/tools/src/main/java/ca/concordia/clac/ml/classifier/StringClassifierLabeller.java
@@ -1,51 +1,6 @@
 package ca.concordia.clac.ml.classifier;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.resource.ResourceInitializationException;
-import org.cleartk.ml.CleartkAnnotator;
-import org.cleartk.ml.DataWriter;
-import org.cleartk.ml.jar.DefaultDataWriterFactory;
-import org.cleartk.ml.jar.GenericJarClassifierFactory;
-
 public class StringClassifierLabeller<INSTANCE_TYPE> extends GenericClassifierLabeller<String, INSTANCE_TYPE>{
 
-	public static <T> AnalysisEngineDescription getWriterDescription(
-			Class<? extends ClassifierAlgorithmFactory<String, T>> classifierAlgorithmFactoryCls,
-					Class<? extends DataWriter<String>> dataWriterCls, File outputDirectory, Object... otherParams) throws ResourceInitializationException{
-		List<Object> params = new ArrayList<>();
-		params.addAll(Arrays.asList(
-				GenericClassifierLabeller.PARAM_LABELER_CLS_NAME, classifierAlgorithmFactoryCls.getName(), 
-				CleartkAnnotator.PARAM_IS_TRAINING, true, 
-				DefaultDataWriterFactory.PARAM_DATA_WRITER_CLASS_NAME, dataWriterCls.getName(),
-				DefaultDataWriterFactory.PARAM_OUTPUT_DIRECTORY, outputDirectory) 
-				);
-		params.addAll(Arrays.asList(otherParams));
-
-		return AnalysisEngineFactory.createEngineDescription(StringClassifierLabeller.class,
-				params.toArray(new Object[params.size()])
-				);
-	}
-
-	public static <T> AnalysisEngineDescription getClassifierDescription(
-			Class<? extends ClassifierAlgorithmFactory<String, T>> classifierAlgorithmFactoryCls, URL modelUrl, Object... otherParams) throws ResourceInitializationException{
-		List<Object> params = new ArrayList<>();
-		params.addAll(Arrays.asList(
-				GenericClassifierLabeller.PARAM_LABELER_CLS_NAME, classifierAlgorithmFactoryCls.getName(), 
-				CleartkAnnotator.PARAM_IS_TRAINING, false, 
-				GenericClassifierLabeller.PARAM_PARALLEL_CLASSIFICATION, false,
-				GenericJarClassifierFactory.PARAM_CLASSIFIER_JAR_PATH, modelUrl)
-				);
-		params.addAll(Arrays.asList(otherParams));
-
-		return AnalysisEngineFactory.createEngineDescription(StringClassifierLabeller.class, 
-				params.toArray(new Object[params.size()])
-				);
-	}
+	
 }
