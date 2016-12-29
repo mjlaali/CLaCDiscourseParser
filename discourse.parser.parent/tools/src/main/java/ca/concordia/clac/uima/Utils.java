008c8936ca2aebc1c601a054dfb126f49a4c4910
Majid Laali <mjlaali@gmail.com>
2016-12-29 11:47:42 -0500

    add utils

---
+++ discourse.parser.parent/tools/src/main/java/ca/concordia/clac/uima/Utils.java
@@ -0,0 +1,36 @@
+package ca.concordia.clac.uima;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Progress;
+
+public class Utils {
+	public static void runWithProgressbar(CollectionReader reader, AnalysisEngineDescription engineDescription)
+			throws UIMAException, IOException, ResourceInitializationException {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				boolean toExit = false;
+				while (!toExit){
+					try {
+						Thread.sleep(2000);
+						Progress[] progresses = reader.getProgress();
+						for (Progress progess: progresses){
+							System.out.println(progess.getCompleted() + "/" + progess.getTotal());
+							if (progess.getCompleted() == progess.getTotal())
+								toExit = true;
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+		SimplePipeline.runPipeline(reader, engineDescription);
+	}
+}
