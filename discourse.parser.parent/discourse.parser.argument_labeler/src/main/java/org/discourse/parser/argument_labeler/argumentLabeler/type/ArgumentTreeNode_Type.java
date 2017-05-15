dcb63ab225c6780e2ba35a6dd95891664a116899
Majid Laali <mjlaali@gmail.com>
2017-05-14 23:39:00 -0400

    remove warnings.

--- discourse.parser.parent/discourse.parser.argument_labeler/src/main/java/org/discourse/parser/argument_labeler/argumentLabeler/type/ArgumentTreeNode_Type.java
+++ discourse.parser.parent/discourse.parser.argument_labeler/src/main/java/org/discourse/parser/argument_labeler/argumentLabeler/type/ArgumentTreeNode_Type.java
@@ -17,103 +17,104 @@ import org.apache.uima.jcas.tcas.Annotation_Type;
  * Updated by JCasGen Fri Apr 15 12:50:10 EDT 2016
  * @generated */
 public class ArgumentTreeNode_Type extends Annotation_Type {
-  /** @generated 
-   * @return the generator for this type
-   */
-  @Override
-  protected FSGenerator getFSGenerator() {return fsGenerator;}
-  /** @generated */
-  private final FSGenerator fsGenerator = 
-    new FSGenerator() {
-      public FeatureStructure createFS(int addr, CASImpl cas) {
-  			 if (ArgumentTreeNode_Type.this.useExistingInstance) {
-  			   // Return eq fs instance if already created
-  		     FeatureStructure fs = ArgumentTreeNode_Type.this.jcas.getJfsFromCaddr(addr);
-  		     if (null == fs) {
-  		       fs = new ArgumentTreeNode(addr, ArgumentTreeNode_Type.this);
-  			   ArgumentTreeNode_Type.this.jcas.putJfsFromCaddr(addr, fs);
-  			   return fs;
-  		     }
-  		     return fs;
-        } else return new ArgumentTreeNode(addr, ArgumentTreeNode_Type.this);
-  	  }
-    };
-  /** @generated */
-  public final static int typeIndexID = ArgumentTreeNode.typeIndexID;
-  /** @generated 
+	/** @generated 
+	 * @return the generator for this type
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	protected FSGenerator getFSGenerator() {return fsGenerator;}
+	/** @generated */
+	@SuppressWarnings("rawtypes")
+	private final FSGenerator fsGenerator = 
+	new FSGenerator() {
+		public FeatureStructure createFS(int addr, CASImpl cas) {
+			if (ArgumentTreeNode_Type.this.useExistingInstance) {
+				// Return eq fs instance if already created
+				FeatureStructure fs = ArgumentTreeNode_Type.this.jcas.getJfsFromCaddr(addr);
+				if (null == fs) {
+					fs = new ArgumentTreeNode(addr, ArgumentTreeNode_Type.this);
+					ArgumentTreeNode_Type.this.jcas.putJfsFromCaddr(addr, fs);
+					return fs;
+				}
+				return fs;
+			} else return new ArgumentTreeNode(addr, ArgumentTreeNode_Type.this);
+		}
+	};
+	/** @generated */
+	public final static int typeIndexID = ArgumentTreeNode.typeIndexID;
+	/** @generated 
      @modifiable */
-  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
- 
-  /** @generated */
-  final Feature casFeat_treeNode;
-  /** @generated */
-  final int     casFeatCode_treeNode;
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
-  public int getTreeNode(int addr) {
-        if (featOkTst && casFeat_treeNode == null)
-      jcas.throwFeatMissing("treeNode", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
-    return ll_cas.ll_getRefValue(addr, casFeatCode_treeNode);
-  }
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
-  public void setTreeNode(int addr, int v) {
-        if (featOkTst && casFeat_treeNode == null)
-      jcas.throwFeatMissing("treeNode", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
-    ll_cas.ll_setRefValue(addr, casFeatCode_treeNode, v);}
-    
-  
- 
-  /** @generated */
-  final Feature casFeat_discourseArgument;
-  /** @generated */
-  final int     casFeatCode_discourseArgument;
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @return the feature value 
-   */ 
-  public int getDiscourseArgument(int addr) {
-        if (featOkTst && casFeat_discourseArgument == null)
-      jcas.throwFeatMissing("discourseArgument", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
-    return ll_cas.ll_getRefValue(addr, casFeatCode_discourseArgument);
-  }
-  /** @generated
-   * @param addr low level Feature Structure reference
-   * @param v value to set 
-   */    
-  public void setDiscourseArgument(int addr, int v) {
-        if (featOkTst && casFeat_discourseArgument == null)
-      jcas.throwFeatMissing("discourseArgument", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
-    ll_cas.ll_setRefValue(addr, casFeatCode_discourseArgument, v);}
-    
-  
+	public final static boolean featOkTst = JCasRegistry.getFeatOkTst("org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
+
+	/** @generated */
+	final Feature casFeat_treeNode;
+	/** @generated */
+	final int     casFeatCode_treeNode;
+	/** @generated
+	 * @param addr low level Feature Structure reference
+	 * @return the feature value 
+	 */ 
+	public int getTreeNode(int addr) {
+		if (featOkTst && casFeat_treeNode == null)
+			jcas.throwFeatMissing("treeNode", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
+		return ll_cas.ll_getRefValue(addr, casFeatCode_treeNode);
+	}
+	/** @generated
+	 * @param addr low level Feature Structure reference
+	 * @param v value to set 
+	 */    
+	public void setTreeNode(int addr, int v) {
+		if (featOkTst && casFeat_treeNode == null)
+			jcas.throwFeatMissing("treeNode", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
+		ll_cas.ll_setRefValue(addr, casFeatCode_treeNode, v);}
 
 
 
-  /** initialize variables to correspond with Cas Type and Features
+	/** @generated */
+	final Feature casFeat_discourseArgument;
+	/** @generated */
+	final int     casFeatCode_discourseArgument;
+	/** @generated
+	 * @param addr low level Feature Structure reference
+	 * @return the feature value 
+	 */ 
+	public int getDiscourseArgument(int addr) {
+		if (featOkTst && casFeat_discourseArgument == null)
+			jcas.throwFeatMissing("discourseArgument", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
+		return ll_cas.ll_getRefValue(addr, casFeatCode_discourseArgument);
+	}
+	/** @generated
+	 * @param addr low level Feature Structure reference
+	 * @param v value to set 
+	 */    
+	public void setDiscourseArgument(int addr, int v) {
+		if (featOkTst && casFeat_discourseArgument == null)
+			jcas.throwFeatMissing("discourseArgument", "org.discourse.parser.argument_labeler.argumentLabeler.type.ArgumentTreeNode");
+		ll_cas.ll_setRefValue(addr, casFeatCode_discourseArgument, v);}
+
+
+
+
+
+	/** initialize variables to correspond with Cas Type and Features
 	 * @generated
 	 * @param jcas JCas
 	 * @param casType Type 
 	 */
-  public ArgumentTreeNode_Type(JCas jcas, Type casType) {
-    super(jcas, casType);
-    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+	public ArgumentTreeNode_Type(JCas jcas, Type casType) {
+		super(jcas, casType);
+		casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
 
- 
-    casFeat_treeNode = jcas.getRequiredFeatureDE(casType, "treeNode", "uima.tcas.Annotation", featOkTst);
-    casFeatCode_treeNode  = (null == casFeat_treeNode) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_treeNode).getCode();
 
- 
-    casFeat_discourseArgument = jcas.getRequiredFeatureDE(casType, "discourseArgument", "org.cleartk.discourse.type.DiscourseArgument", featOkTst);
-    casFeatCode_discourseArgument  = (null == casFeat_discourseArgument) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_discourseArgument).getCode();
+		casFeat_treeNode = jcas.getRequiredFeatureDE(casType, "treeNode", "uima.tcas.Annotation", featOkTst);
+		casFeatCode_treeNode  = (null == casFeat_treeNode) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_treeNode).getCode();
 
-  }
+
+		casFeat_discourseArgument = jcas.getRequiredFeatureDE(casType, "discourseArgument", "org.cleartk.discourse.type.DiscourseArgument", featOkTst);
+		casFeatCode_discourseArgument  = (null == casFeat_discourseArgument) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_discourseArgument).getCode();
+
+	}
 }
 
 
 
-    
