e365cd2be233ef05ba256431863fd7d6be8b3e21
Majid Laali <mjlaali@gmail.com>
2017-07-08 23:45:22 -0400

    update train.sh

--- discourse.parser.parent/discourse.parser.dc-disambiguation/train.sh
+++ discourse.parser.parent/discourse.parser.dc-disambiguation/train.sh
@@ -7,8 +7,8 @@ mkdir outputs/resources
 cp src/main/resources/clacParser/model/dcHeadList.txt outputs/resources
 cp src/main/resources/clacParser/model/eng_sm5.gr outputs/resources
 
-java -cp target/discourse.parser.dc-disambiguation-0.1.1.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseVsNonDiscourseClassifier 
-java -cp target/discourse.parser.dc-disambiguation-0.1.1.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseSenseLabeler
-java -cp target/discourse.parser.dc-disambiguation-0.1.1.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseConnectiveDisambiguator -m outputs/resources
+java -cp target/discourse.parser.dc-disambiguation-2.1.0-SNAPSHOT.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseVsNonDiscourseClassifier 
+java -cp target/discourse.parser.dc-disambiguation-2.1.0-SNAPSHOT.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseSenseLabeler
+java -cp target/discourse.parser.dc-disambiguation-2.1.0-SNAPSHOT.jar:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseConnectiveDisambiguator -m outputs/resources
 
  
