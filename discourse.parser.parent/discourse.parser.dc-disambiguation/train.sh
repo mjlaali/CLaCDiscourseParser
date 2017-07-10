2b145cb0d03384763949e78ecd7d9816718137a4
Majid Laali <mjlaali@gmail.com>
2017-07-10 11:32:43 -0400

    increase java heap size

--- discourse.parser.parent/discourse.parser.dc-disambiguation/train.sh
+++ discourse.parser.parent/discourse.parser.dc-disambiguation/train.sh
@@ -9,6 +9,6 @@ cp src/main/resources/clacParser/model/eng_sm5.gr outputs/resources
 
 java -cp target/*:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseVsNonDiscourseClassifier
 java -cp target/*:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseSenseLabeler
-java -cp target/*:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseConnectiveDisambiguator -m outputs/resources -c "$1"
+java -Xmx16G -cp target/*:target/dependency/* ca.concordia.clac.discourse.parser.dc.disambiguation.DiscourseConnectiveDisambiguator -m outputs/resources -c "$1"
 
 cp -rf outputs/resources/ src/main/resources/clacParser/model
